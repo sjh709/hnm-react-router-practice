5c9d62fcb2381168ed4b603303bcc9875ebc752c
Jihee <sjh709@naver.com>
2024-01-14 15:40:03 +0900

    기능추가: 전체 상품 보여주기

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const ProductCard = () => {
+const ProductCard = ({ item }) => {
   return (
-    <div>
-      <img src='' />
-      <div>Conscious choice</div>
-      <div>벨티드 트윌 코트</div>
-      <div>₩99900</div>
-      <div>신제품</div>
+    <div className='card'>
+      <img src={item?.img} />
+      <div className='choice'>
+        {item?.choice === true ? 'Conscious choice' : ''}
+      </div>
+      <div>{item?.title}</div>
+      <div>₩{item?.price}</div>
+      <div className='new-product'>{item?.new === true ? '신제품' : ''}</div>
     </div>
   );
 };
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
@@ -14,7 +15,15 @@ const ProductAll = () => {
   }, []);
   return (
     <div>
-      <ProductCard />
+      <Container>
+        <Row>
+          {productList.map((menu) => (
+            <Col md={3} sm={12} key={menu.id}>
+              <ProductCard item={menu} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
