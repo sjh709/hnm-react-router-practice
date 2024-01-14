64237183f5f417be8cbe83e1ddc9d10c0456c820
Jihee <sjh709@naver.com>
2024-01-14 15:55:02 +0900

    기능추가: 로그인 페이지(기능X)

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Form, Button, Container } from 'react-bootstrap';
 
 const Login = () => {
-  return <div>로그인 페이지</div>;
+  return (
+    <Container className='login-area'>
+      <Form className='login-form'>
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type='email' placeholder='Enter email' />
+        </Form.Group>
+
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' placeholder='Password' />
+        </Form.Group>
+        <Button variant='danger' type='submit'>
+          로그인
+        </Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default Login;
