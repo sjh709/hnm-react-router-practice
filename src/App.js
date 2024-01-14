ab5515471fa2f77d2d6b832613ab0e5ed5d3d802
Jihee <sjh709@naver.com>
2024-01-14 17:35:03 +0900

    기능추가: Private Route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from './page/Login';
 import ProductDetail from './page/ProductDetail';
 import Navbar from './component/Navbar';
 import { useEffect, useState } from 'react';
+import PrivateRoute from './route/PrivateRoute';
 
 // 1. 전체상품페이지, 로그인페이지, 상품상세페이지
 // 1-1. 네비게이션 바
@@ -31,7 +32,10 @@ function App() {
           path='/login'
           element={<Login setAuthenticate={setAuthenticate} />}
         />
-        <Route path='/product/:id' element={<ProductDetail />} />
+        <Route
+          path='/product/:id'
+          element={<PrivateRoute authenticate={authenticate} />}
+        />
       </Routes>
     </div>
   );
