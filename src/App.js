3232f1a80201a4171de8e21ced78a68eb4b7a70d
Jihee <sjh709@naver.com>
2024-01-14 17:13:13 +0900

    기능추가: 로그인 기능

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ProductAll from './page/ProductAll';
 import Login from './page/Login';
 import ProductDetail from './page/ProductDetail';
 import Navbar from './component/Navbar';
+import { useEffect, useState } from 'react';
 
 // 1. 전체상품페이지, 로그인페이지, 상품상세페이지
 // 1-1. 네비게이션 바
@@ -17,12 +18,19 @@ import Navbar from './component/Navbar';
 // 8. 로그인을 하면 로그아웃이 보이고 로그아웃을 하면 로그인이 보인다.
 // 9. 상품을 검색할 수 있다.
 function App() {
+  const [authenticate, setAuthenticate] = useState(false); // true면 로그인이 됨, false면 로그인이 안됨
+  useEffect(() => {
+    console.log('authenticate', authenticate);
+  }, [authenticate]);
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path='/' element={<ProductAll />} />
-        <Route path='/login' element={<Login />} />
+        <Route
+          path='/login'
+          element={<Login setAuthenticate={setAuthenticate} />}
+        />
         <Route path='/product/:id' element={<ProductDetail />} />
       </Routes>
     </div>
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({ setAuthenticate }) => {
+  const navigate = useNavigate();
+  const loginUser = (event) => {
+    event.preventDefault();
+    setAuthenticate(true);
+    navigate('/');
+  };
   return (
     <Container className='login-area'>
-      <Form className='login-form'>
+      <Form className='login-form' onSubmit={(event) => loginUser(event)}>
         <Form.Group className='mb-3' controlId='formBasicEmail'>
           <Form.Label>Email address</Form.Label>
           <Form.Control type='email' placeholder='Enter email' />
