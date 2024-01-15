b40f86ceed0d14e90d42f14cc8c3ac209e71812a
Jihee <sjh709@naver.com>
2024-01-15 18:21:56 +0900

    추가: 미들웨어 기본세팅

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
 import productReducer from './reducer/productReducer';
 
-let store = createStore(productReducer);
+let store = createStore(productReducer, applyMiddleware(thunk));
 
 export default store;
