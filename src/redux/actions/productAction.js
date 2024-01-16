a21b69169363b9b69d98953dcffb413fecbe5985
Jihee <sjh709@naver.com>
2024-01-16 14:40:05 +0900

    수정: 상품리스트 받아오는 부분 redux toolkit 변경

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -1,9 +1,12 @@
+import { productActions } from '../reducers/productReducer';
+
 function getProducts(searchQuery) {
   return async (dispatch, getState) => {
     let url = `https://my-json-server.typicode.com/sjh709/hnm-react-router-practice/products/?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
-    dispatch({ type: 'GET_PRODUCT_SUCCESS', payload: { data } });
+    // dispatch({ type: 'GET_PRODUCT_SUCCESS', payload: { data } });
+    dispatch(productActions.getAllProducts({ data }));
   };
 }
 
@@ -12,7 +15,8 @@ function getProductDetail(id) {
     let url = `https://my-json-server.typicode.com/sjh709/hnm-react-router-practice/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
-    dispatch({ type: 'GET_SINGLE_PRODUCT_SUCCESS', payload: { data } });
+    // dispatch({ type: 'GET_SINGLE_PRODUCT_SUCCESS', payload: { data } });
+    dispatch(productActions.getSingleProduct({ data }));
   };
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,25 @@
-import { createStore, applyMiddleware } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-import { composeWithDevTools } from '@redux-devtools/extension';
-import { thunk } from 'redux-thunk';
-import rootReducer from './reducers';
+// import { createStore, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from '@redux-devtools/extension';
+// import { thunk } from 'redux-thunk';
+// import rootReducer from './reducers';
+import { configureStore } from '@reduxjs/toolkit';
+import productReducer from './reducers/productReducer';
+import authenticateReducer from './reducers/authenticateReducer';
 
-let store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+// let store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+// combinereducer
+// thunk
+// applyMiddleware
+// composeWithDevTools
+
+const store = configureStore({
+  reducer: {
+    auth: authenticateReducer,
+    product: productReducer,
+  },
+});
 
 export default store;
