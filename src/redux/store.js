3b1789ca8ab81ea1bc3445d718a657ee566720f4
Jihee <sjh709@naver.com>
2024-01-16 11:12:13 +0900

    수정: combineReducer - reducer 여러개 합치기

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
-import productReducer from './reducer/productReducer';
+import rootReducer from './reducers';
 
-let store = createStore(productReducer, applyMiddleware(thunk));
+let store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
